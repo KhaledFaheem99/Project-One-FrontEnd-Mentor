5ac9fdecc57cf1d98736dc1de6ae75a30bb92b3c
Khaled Faheem <khaledfaheem99@gmail.com>
2024-02-25 15:21:13 +0200

    replace minus icon to plus icon when click it

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ let answers = document.querySelectorAll(".holder .answer");
 
 pluss.forEach((plus, index) => {
   plus.addEventListener("click", (e) => {
-    if (e.currentTarget.src.includes("plus")) {
+    answers.forEach((answer) => {
+      if (answer !== answers[index]) {
+        answer.classList.remove("show");
+      }
+    });
+
+    pluss.forEach((icon) => {
+      if (icon !== e.currentTarget) {
+        icon.src = "/assets/images/icon-plus.svg";
+      }
+    });
+
+    if (e.currentTarget.src.includes("icon-plus")) {
       e.currentTarget.src = "/assets/images/icon-minus.svg";
     } else {
       e.currentTarget.src = "/assets/images/icon-plus.svg";
     }
-    answers.forEach((answer) => {
-      answer.classList.remove("show");
-    });
+
     answers[index].classList.toggle("show");
   });
 });
